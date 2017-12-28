b79f01b2b0a332ece3fccefe412df9037ce97435
Callum Rogers <callumrogers@hotmail.co.uk>
2017-12-28 23:40:47 +0000

    Demonstrate the problem we're trying to fix

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,12 +6,12 @@ import Data.Text.Lazy.IO as T
 
 import LLVM.Pretty (ppllvm)
 import LLVM.AST (Module)
-import LLVM.AST.Type (i32, void)
+import LLVM.AST.Type (i32, half, void)
 
 import LLVM.IRBuilder.Constant (int32)
 import LLVM.IRBuilder.Module (buildModule, function)
 import LLVM.IRBuilder.Monad (block, named)
-import LLVM.IRBuilder.Instruction (add, ret)
+import LLVM.IRBuilder.Instruction (add, ret, unreachable)
 
 import LLVM.Context (withContext)
 import LLVM.Module (File(..), withModuleFromAST, writeObjectToFile)
@@ -19,13 +19,10 @@ import LLVM.Target (initializeAllTargets, withHostTargetMachine)
 
 myModule :: Module
 myModule = buildModule "exampleModule" $ do
-    function "add" [(i32, "a"), (i32, "b")] i32 $ \[a, b] -> do
+    function "add" [(i32, "a"), (half, "b")] i32 $ \[a, b] -> do
         entry <- block `named` "entry"; do
             c <- add a b
             ret c
-
-    function "main" [] i32 $ \[] -> do
-        ret =<< int32 0
 
 printModule :: IO ()
 printModule = T.putStrLn $ ppllvm myModule
