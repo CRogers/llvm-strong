c12d85ee99b59db532fadc8559efefb1e66da601
Callum Rogers <callumrogers@hotmail.co.uk>
2017-12-28 23:14:23 +0000

    Write out some object code woo

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,24 +1,39 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Lib
-    ( someFunc
-    ) where
+module Lib where
         
 import Data.Text.Lazy.IO as T
 
-import LLVM.Pretty
-import LLVM.AST hiding (function)
-import LLVM.AST.Type as AST
-import qualified LLVM.AST.Float as F
-import qualified LLVM.AST.Constant as C
+import LLVM.Pretty (ppllvm)
+import LLVM.AST (Module)
+import LLVM.AST.Type (i32, void)
 
-import LLVM.IRBuilder.Module
-import LLVM.IRBuilder.Monad
-import LLVM.IRBuilder.Instruction
+import LLVM.IRBuilder.Constant (int32)
+import LLVM.IRBuilder.Module (buildModule, function)
+import LLVM.IRBuilder.Monad (block, named)
+import LLVM.IRBuilder.Instruction (add, ret)
 
-someFunc :: IO ()
-someFunc = T.putStrLn $ ppllvm $ buildModule "exampleModule" $ do
+import LLVM.Context (withContext)
+import LLVM.Module (File(..), withModuleFromAST, writeObjectToFile)
+import LLVM.Target (initializeAllTargets, withHostTargetMachine)
+
+myModule :: Module
+myModule = buildModule "exampleModule" $ do
     function "add" [(i32, "a"), (i32, "b")] i32 $ \[a, b] -> do
         entry <- block `named` "entry"; do
             c <- add a b
             ret c
+
+    function "main" [] i32 $ \[] -> do
+        ret =<< int32 0
+
+printModule :: IO ()
+printModule = T.putStrLn $ ppllvm myModule
+
+outputMachineCode :: IO ()
+outputMachineCode = do
+    initializeAllTargets
+    withContext $ \context -> do
+        withHostTargetMachine $ \targetMachine -> do
+            withModuleFromAST context myModule $ \llvmModule ->
+                writeObjectToFile targetMachine (File "foo") llvmModule
