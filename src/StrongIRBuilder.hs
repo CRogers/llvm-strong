df1a7544a0f482f9b0cf3ff279245e8c255d7c82
Callum Rogers <callumrogers@hotmail.co.uk>
2017-12-29 00:11:18 +0000

    Play around with RebindableSyntax

--- src/StrongIRBuilder.hs
+++ src/StrongIRBuilder.hs
@@ -1,2 +1,18 @@
+{-# LANGUAGE RebindableSyntax #-}
+
 module StrongIRBuilder where
+
+import Prelude hiding ((>>), (>>=), return)
+
+import Control.Monad.Indexed (IxMonad(..), IxPointed(..), (>>>=), ireturn)
+
+return :: IxPointed m => a -> m i i a
+return = ireturn
+
+(>>=) :: IxMonad m => m i j a -> (a -> m j k b) -> m i k b
+(>>=) = (>>>=)
+
+data IRBuilderState = Middle | Terminator
+
+data StrongIRBuilder s = StrongIRBuilder
 
