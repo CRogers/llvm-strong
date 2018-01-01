2b655dd35caf7db3f4abc9a2efa3c358d033ae92
Callum Rogers <callumrogers@hotmail.co.uk>
2018-01-01 13:53:45 +0000

    Remov ShouldNotTypecheck for now, add a real test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,15 @@
 {-# OPTIONS_GHC -fdefer-type-errors #-}
 
 import Test.Hspec (hspec, describe, it)
-import Test.ShouldNotTypecheck (shouldNotTypecheck)
 
 main :: IO ()
 main = hspec $ do
-    describe "Type Tests" $ do
-        it "should not typecheck" $ do
-            shouldNotTypecheck ()
+    describe "StrongIRBuilder" $ do
+        it "should make a function that can add two i32s together" $ do
+            let pureModule = buildModule "exampleModule" $ do
+                function "add" [(i32, "a"), (i32, "b")] i32 $ \[a, b] -> do
+                    block `named` "entry"; do
+                        c <- add a b
+                        ret c
+
+            runM
