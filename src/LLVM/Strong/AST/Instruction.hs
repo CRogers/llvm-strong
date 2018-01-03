ace4f7fb70fb9552b44fa63698d22b967e698fc4
Callum Rogers <callumrogers@hotmail.co.uk>
2018-01-03 18:27:17 +0000

    Add instructions and constants

--- src/LLVM/Strong/AST/Instruction.hs
+++ src/LLVM/Strong/AST/Instruction.hs
@@ -29,6 +29,7 @@ type Name = String
 
 data Operand :: LlvmType -> Type where
     LocalReference :: Name -> SLlvmType ty -> Operand ty
+    ConstantOperand :: Constant ty -> Operand ty
 
 refToI8 :: Operand ('Integer 8)
 refToI8 = LocalReference "s" i8
@@ -36,14 +37,33 @@ refToI8 = LocalReference "s" i8
 voidType :: SLlvmType 'Void
 voidType = SVoid
 
+intType :: KnownNat n => SLlvmType ('Integer n)
+intType = SInteger SNat
+
 i1 :: SLlvmType ('Integer 1)
-i1 = SInteger (SNat @1)
+i1 = intType @1
 
 i8 :: SLlvmType ('Integer 8)
-i8 = SInteger (SNat @8)
+i8 = intType @8
+
+i32 :: SLlvmType ('Integer 32)
+i32 = intType @32
 
 pointerToI8 :: SLlvmType ('Pointer ('Integer 8))
 pointerToI8 = SPointer i8
 
 pointerToPointerToPointerToI8 :: SLlvmType ('Pointer ('Pointer ('Pointer ('Integer 8))))
 pointerToPointerToPointerToI8 = SPointer (SPointer (SPointer i8))
+
+data Instruction :: LlvmType -> Type where
+    Add :: Operand ('Integer i) -> Operand ('Integer i) -> Instruction ('Integer i)
+    Alloca :: SLlvmType ty -> Operand ('Integer 32) -> Instruction ('Pointer ty)
+
+data Constant :: LlvmType -> Type where
+    Int :: SLlvmType ('Integer i) -> Integer -> Constant ('Integer i)
+
+x =
+    let a = LocalReference "lol" i8 in
+    let b = ConstantOperand $ Int i8 4 in
+    let c = Add a b in
+    Alloca i8 (ConstantOperand $ Int i32 10)
