25b67111c2ca014cc9bbfb170b494352429ae7c1
Callum Rogers <callumrogers@hotmail.co.uk>
2018-01-03 17:10:30 +0000

    Implement a number of LLVM type

--- src/LLVM/Strong/AST/Instruction.hs
+++ src/LLVM/Strong/AST/Instruction.hs
@@ -1,26 +1,49 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeApplications #-}
-
+{-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeInType #-}
 
 module LLVM.Strong.AST.Instruction where
 
+import Data.Singletons (SingI, Sing)
+import Data.Singletons.TypeLits
 import Data.Singletons.TH (singletons)
-import Data.Nat (Nat(..), Lit, sLit)
+import qualified GHC.TypeLits as TypeLits
+import Data.Kind (Type)
 
 $(singletons [d|
 
-    data Type 
+    -- See https://stackoverflow.com/a/36562893/139766
+    data LlvmType_ nat
         = Void
-        | Integer Nat
+        | Integer nat
+        | Pointer (LlvmType_ nat)
+        | Function (LlvmType_ nat) [LlvmType_ nat]
 
+    data Test = Foo [Bool]
     |])
 
-voidType :: SType 'Void
+type LlvmType = LlvmType_ Nat
+type SLlvmType = SLlvmType_
+
+type Name = String
+
+data Operand :: LlvmType -> Type where
+    LocalReference :: Name -> SLlvmType ty -> Operand ty
+
+refToI8 :: Operand ('Integer 8)
+refToI8 = LocalReference "s" i8
+
+voidType :: SLlvmType 'Void
 voidType = SVoid
 
-i1 :: SType ('Integer (Lit 1))
-i1 = SInteger (sLit @1)
+i1 :: SLlvmType ('Integer 1)
+i1 = SInteger (SNat @1)
+
+i8 :: SLlvmType ('Integer 8)
+i8 = SInteger (SNat @8)
+
+pointerToI8 :: SLlvmType ('Pointer ('Integer 8))
+pointerToI8 = SPointer i8
+
+pointerToPointerToPointerToI8 :: SLlvmType ('Pointer ('Pointer ('Pointer ('Integer 8))))
+pointerToPointerToPointerToI8 = SPointer (SPointer (SPointer i8))
